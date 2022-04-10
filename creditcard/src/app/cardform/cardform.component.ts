283c36695331990f972c656545c6372ff03b36f2
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-04-10 17:55:20 +0300

    FormControl-Validators

--- creditcard/src/app/cardform/cardform.component.ts
+++ creditcard/src/app/cardform/cardform.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-cardform',
   templateUrl: './cardform.component.html',
   styleUrls: ['./cardform.component.scss']
 })
 export class CardformComponent implements OnInit {
+
+  cardForm = new FormGroup({
+    name: new FormControl(null,
+      [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+  })
 
   constructor() { }
 
