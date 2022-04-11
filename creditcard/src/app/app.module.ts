946df58b549b71b6ef2ce526fd4b55a62abae06d
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-04-11 06:03:10 +0300

    porting - input component

--- creditcard/src/app/app.module.ts
+++ creditcard/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardformComponent } from './cardform/cardform.component';
+import { InputComponent } from './input/input.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardformComponent
+    CardformComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
